2c541d9e136db6d3a9624e1d5abb8cbb94ce4cbd
Alex Cheng <alex.kpc@gmail.com>
2017-07-11 23:54:45 +0800

    Update the code to loop through results

--- cloud-client/src/main/java/com/example/bigquery/SimpleApp.java
+++ cloud-client/src/main/java/com/example/bigquery/SimpleApp.java
@@ -16,87 +16,85 @@
 
 package com.example.bigquery;
 
-import com.google.cloud.bigquery.QueryJobConfiguration;
+import com.google.cloud.bigquery.*;
 
 // [START all]
 // [START imports]
 import com.google.auth.oauth2.ServiceAccountCredentials;
-import com.google.cloud.bigquery.BigQuery;
-import com.google.cloud.bigquery.BigQueryOptions;
-import com.google.cloud.bigquery.FieldValue;
-import com.google.cloud.bigquery.Job;
-import com.google.cloud.bigquery.JobId;
-import com.google.cloud.bigquery.JobInfo;
 import com.google.cloud.bigquery.QueryJobConfiguration;
-import com.google.cloud.bigquery.QueryResponse;
-import com.google.cloud.bigquery.QueryResult;
 
 import java.io.FileInputStream;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 // [END imports]
 
 public class SimpleApp {
-  public static void main(String... args) throws Exception {
-    // [START create_client]
-    // BigQuery bigquery = BigQueryOptions.getDefaultInstance().getService();
-      BigQuery bigquery = BigQueryOptions.newBuilder().setProjectId("corded-vim-168917")
-              .setCredentials(ServiceAccountCredentials.fromStream(new FileInputStream("key.json")))
-              .build().getService();
-    // [END create_client]
-    // [START run_query]
-    QueryJobConfiguration queryConfig =
-        QueryJobConfiguration.newBuilder(
-                "SELECT * FROM `corded-vim-168917.FX.epl` "
-                    + "WHERE HomeTeam='Man United';")
-            // Use standard SQL syntax for queries.
-            // See: https://cloud.google.com/bigquery/sql-reference/
-            .setUseLegacySql(false)
-            .build();
+    public static void main(String... args) throws Exception {
+        // [START create_client]
+        // BigQuery bigquery = BigQueryOptions.getDefaultInstance().getService();
+        BigQuery bigquery = BigQueryOptions.newBuilder().setProjectId("corded-vim-168917")
+                .setCredentials(ServiceAccountCredentials.fromStream(new FileInputStream("key.json")))
+                .build().getService();
+        // [END create_client]
+        // [START run_query]
+        QueryJobConfiguration queryConfig =
+                QueryJobConfiguration.newBuilder(
+                        "SELECT * FROM `corded-vim-168917.FX.epl` "
+                                + "WHERE HomeTeam='Man United';")
+                        // Use standard SQL syntax for queries.
+                        // See: https://cloud.google.com/bigquery/sql-reference/
+                        .setUseLegacySql(false)
+                        .build();
 
-    // Create a job ID so that we can safely retry.
-    JobId jobId = JobId.of(UUID.randomUUID().toString());
-    Job queryJob = bigquery.create(JobInfo.newBuilder(queryConfig).setJobId(jobId).build());
+        // Create a job ID so that we can safely retry.
+        JobId jobId = JobId.of(UUID.randomUUID().toString());
+        Job queryJob = bigquery.create(JobInfo.newBuilder(queryConfig).setJobId(jobId).build());
 
-    // Wait for the query to complete.
-    queryJob = queryJob.waitFor();
+        // Wait for the query to complete.
+        queryJob = queryJob.waitFor();
 
-    // Check for errors
-    if (queryJob == null) {
-      throw new RuntimeException("Job no longer exists");
-    } else if (queryJob.getStatus().getError() != null) {
-      // You can also look at queryJob.getStatus().getExecutionErrors() for all
-      // errors, not just the latest one.
-      throw new RuntimeException(queryJob.getStatus().getError().toString());
-    }
-
-    // Get the results.
-    QueryResponse response = bigquery.getQueryResults(jobId);
-    // [END run_query]
-
-    // [START print_results]
-    QueryResult result = response.getResult();
-
-    // Print all pages of the results.
-    while (result != null) {
-      for (List<FieldValue> row : result.iterateAll()) {
-        List<FieldValue> titles = row.get(0).getRepeatedValue();
-        System.out.println("titles:");
-
-        for (FieldValue titleValue : titles) {
-          List<FieldValue> titleRecord = titleValue.getRecordValue();
-          String title = titleRecord.get(0).getStringValue();
-          long uniqueWords = titleRecord.get(1).getLongValue();
-          System.out.printf("\t%s: %d\n", title, uniqueWords);
+        // Check for errors
+        if (queryJob == null) {
+            throw new RuntimeException("Job no longer exists");
+        } else if (queryJob.getStatus().getError() != null) {
+            // You can also look at queryJob.getStatus().getExecutionErrors() for all
+            // errors, not just the latest one.
+            throw new RuntimeException(queryJob.getStatus().getError().toString());
         }
 
-        long uniqueWords = row.get(1).getLongValue();
-        System.out.printf("total unique words: %d\n", uniqueWords);
-      }
+        // Get the results.
+        QueryResponse response = bigquery.getQueryResults(jobId);
+        // [END run_query]
 
-      result = result.getNextPage();
+        // [START print_results]
+        QueryResult result = response.getResult();
+
+        // Print all pages of the results.
+        if (result != null) {
+
+            // Print Headers
+            List<Field> headers = result.getSchema().getFields();
+            for (Field name : headers) {
+                System.out.print(name.getName() + ",");
+            }
+
+            // Print Data
+            System.out.println();
+            while (result != null) {
+                for (List<FieldValue> row : result.iterateAll()) {
+                    for (FieldValue val : row) {
+                        System.out.print(val.getStringValue() + ",");
+                    }
+                    System.out.println();
+                }
+                result = result.getNextPage();
+            }
+
+
+        }
+        System.out.print("END");
     }
     // [END print_results]
-  }
 }
 // [END all]
